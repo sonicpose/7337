630f552532d32c66834a35d1a097733a054f9bc0
Dan Roebuck <danroebuck@mac.com>
2017-05-31 19:26:56 +0100

    First commit Grid inishilised

---
+++ main.py
@@ -0,0 +1,21 @@
+def main():
+
+	return
+
+class Grid():
+	def __init__(self):
+		self.width = 100
+		self.height = 100
+
+		heighti = 0
+
+		self.grid = ['']*width
+		while heighti > height:
+			grid[heighti] = grid[heighti] + ['']*width
+			heighti = heighti + 1
+
+	def getCell():
+
+		return cell
+
+main()
